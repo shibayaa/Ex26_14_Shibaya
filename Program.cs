cd3eedb247cd98008c6b643307080025f1178124
Shibayaa <kurobii528@gmail.coom>
2022-07-27 14:39:52 +0900

    様々な

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using InputUtilitys;
-namespace Ex26_14_Shibaya
+namespace Ex27_14_Shibaya
 {
     internal class Program
     {
@@ -10,8 +10,20 @@ namespace Ex26_14_Shibaya
             var depth = InputUtility.InputFloat("奥行");
             Box box = new Box(width, height, depth);
             Console.WriteLine($"boxの表面積={box.GetSurface()}\nboxの体積={box.GetVolume()}");
+
+            var radius = InputUtility.InputFloat("半径");
+            Cylinder cylinder = new Cylinder(radius, height);
+            Console.WriteLine($"円柱の表面積={box.GetSurface()}\n円柱の体積={box.GetVolume()}");
+
+            Sphere sphere = new Sphere(radius);
+            Console.WriteLine($"球の表面積={box.GetSurface()}\n球の体積={box.GetVolume()}");
+
+            var bottom = InputUtility.InputFloat("三角形の底辺");
+            var top = InputUtility.InputFloat("三角形の高さ");
+            TriangularPrism triangularprism = new TriangularPrism(bottom, top, height);
+            Console.WriteLine($"三角柱の表面積={box.GetSurface()}\n三角柱の体積={box.GetVolume()}");
         }
-            class Box
+        class Box
         {
             private float width;
             private float height;
@@ -22,24 +34,75 @@ namespace Ex26_14_Shibaya
                 this.height = h;
                 this.depth = d;
             }
-            public Box(float w = 1)
+            public float GetVolume()
             {
-                this.width = w;
-                this.height = w;
-                this.depth = w;
+                return width * height * depth;
+            }
+            public float GetSurface()
+            {
+                return (width * height + height * depth + depth * width) * 2;
+            }
+        }
+        class Cylinder
+        {
+            private float radius;
+            private float height;
+
+            public Cylinder(float r, float h)
+            {
+                this.radius = r;
+                this.height = h;
             }
 
-        
-        public float GetVolume()
-        {
-            return width * height * depth;
+            public float GetVolume()
+            {
+                return radius * radius * (float)Math.PI * height;
+            }
+            public float GetSurface()
+            {
+                return (radius * radius * (float)Math.PI) * 2 + (radius * 2 * (float)Math.PI * height);
+            }
         }
-        public float GetSurface()
+        class Sphere
         {
-            return (width * height + height * depth + depth * width) * 2;
-        }
-             }
+            private float radius;
+            public Sphere(float r)
+            {
+                this.radius = r;
+            }
 
+            public float GetVolume()
+            {
+                return 4 * (float)Math.PI / 3 * radius * radius * radius;
+            }
+            public float GetSurface()
+            {
+                return (4 * (float)Math.PI * radius * radius);
+            }
+        }
+        class TriangularPrism
+
+        {
+            private float bottom;
+            private float top;
+            private float height;
+
+            public TriangularPrism(float r, float t, float h)
+            {
+                this.bottom = r;
+                this.top = t;
+                this.height = h;
+            }
+
+            public float GetVolume()
+            {
+                return (bottom * top) / 2 * height;
+            }
+            public float GetSurface()
+            {
+                return bottom * top + (bottom + top + (float)Math.Sqrt(bottom * bottom + top * top)) * height;
+            }
+        }
     }
-
 }
+
